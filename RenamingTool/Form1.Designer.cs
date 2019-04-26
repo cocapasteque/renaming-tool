bf98d0ee6ce7207d27897bf70382e2f5ef420ff9
Antoine Thiry <a.thiry@str8labs.com>
2019-04-26 22:56:25 +0200

    Initial commit

---
+++ RenamingTool/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace RenamingTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.path = new System.Windows.Forms.TextBox();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.toReplace = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.replaceWith = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apply = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(725, 3);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(102, 32);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // path
+            // 
+            this.path.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.path.Location = new System.Drawing.Point(3, 3);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(716, 20);
+            this.path.TabIndex = 1;
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.toReplace,
+            this.replaceWith});
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.Location = new System.Drawing.Point(3, 41);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(716, 512);
+            this.grid.TabIndex = 12;
+            // 
+            // toReplace
+            // 
+            this.toReplace.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.toReplace.HeaderText = "To Replace";
+            this.toReplace.Name = "toReplace";
+            // 
+            // replaceWith
+            // 
+            this.replaceWith.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.replaceWith.HeaderText = "Replace With";
+            this.replaceWith.Name = "replaceWith";
+            // 
+            // apply
+            // 
+            this.apply.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.apply.Location = new System.Drawing.Point(3, 559);
+            this.apply.Name = "apply";
+            this.apply.Size = new System.Drawing.Size(716, 25);
+            this.apply.TabIndex = 11;
+            this.apply.Text = "Apply";
+            this.apply.UseVisualStyleBackColor = true;
+            this.apply.Click += new System.EventHandler(this.Apply_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 86.18127F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13.81872F));
+            this.tableLayoutPanel1.Controls.Add(this.apply, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.browseButton, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.grid, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.path, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.98152F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.01848F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(838, 587);
+            this.tableLayoutPanel1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 611);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Super awesome renaming tool of the death";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button apply;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn toReplace;
+        private System.Windows.Forms.DataGridViewTextBoxColumn replaceWith;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ RenamingTool/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenamingTool
+{
+    public partial class Form1 : Form
+    {
+        private List<string> files;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            files = new List<string>();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void Apply_Click(object sender, EventArgs e)
+        {
+            var files = Directory.GetFiles(path.Text);
+            foreach (DataGridViewRow row in grid.Rows)
+            {
+                string toReplace = (string)row.Cells[0].Value;
+                string replaceWith = (string)row.Cells[1].Value;
+
+                if (string.IsNullOrEmpty(toReplace)) continue;
+                if (string.IsNullOrEmpty(replaceWith)) replaceWith = string.Empty;
+
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FileInfo info = new FileInfo(files[i]);
+                    var oldPath = info.FullName;
+                    var newPath = Path.Combine(info.DirectoryName, info.Name.Replace(toReplace, replaceWith));
+                    File.Move(oldPath, newPath);
+                    files[i] = newPath;
+                }
+            }
+        }
+    }
+}
